56763659a06fd0a17b9f9dbbd61cbb63b954f447
SoftSquad <biuro@softsquad.pl>
2020-11-26 01:40:25 +0100

    configuration

---
+++ src/router/routes.js
@@ -0,0 +1,26 @@
+
+const routes = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Index.vue') }
+    ]
+  },
+  {
+    path: '/account',
+    component: () => import('layouts/UserLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/account/Index.vue') }
+    ]
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  }
+]
+
+export default routes
